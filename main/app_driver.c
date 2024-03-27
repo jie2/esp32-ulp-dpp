13e389461e7778f3e0afc71d5d66eee2528cb0c9
jie2 <78070228+jie2@users.noreply.github.com>
2024-03-27 12:38:19 +0000

    Update app_driver.c

--- main/app_driver.c
+++ main/app_driver.c
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+#include <inttypes.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/timers.h>
@@ -25,7 +26,9 @@
 #include <ws2812_led.h>
 #include "app_priv.h"
 
+#include <string.h>
 
+#define STORAGE_SIZE 5
 
 static TimerHandle_t sensor_timer; 
 
@@ -34,15 +37,12 @@ float tsens_value;
 static float g_adc;
 static float g_temp;
 static float g_charge;
-
+char g_past_adc[100]; //change 100 to max size of string
+void floatArrayToString(uint32_t arr[], int size, char *str);
 
 
 void app_sensor_update()
 {
-    ulp_adc_reading &= UINT16_MAX;
-    g_adc = ulp_adc_reading;
-    g_temp = tsens_value;
-
     esp_rmaker_param_update_and_report(
             esp_rmaker_device_get_param_by_type(dpp_device, ESP_RMAKER_PARAM_TEMPERATURE),
             esp_rmaker_float(g_temp)
@@ -57,6 +57,12 @@ void app_sensor_update()
             esp_rmaker_device_get_param_by_name(dpp_device, "State of Charge"),
             esp_rmaker_float(g_charge)
     );
+
+    esp_rmaker_param_update_and_report(
+            esp_rmaker_device_get_param_by_name(dpp_device, "Past ADC Readings"),
+            esp_rmaker_array(g_past_adc)
+    );
+
 }
 
 float app_get_measurement()
@@ -75,13 +81,19 @@ float app_get_measurement()
     ESP_ERROR_CHECK(temperature_sensor_enable(temp_sensor));
     ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
 
+
+    floatArrayToString(ulp_adc_reading, STORAGE_SIZE, g_past_adc); // Convert array to string to upload as JSON array
+
+    //printf("%s\n", g_past_adc);
+    
+
+
     g_temp = tsens_value;
     g_charge = ulp_charge_state; 
+    g_adc = ulp_adc_reading[STORAGE_SIZE-1];
 
-    ulp_adc_reading &= UINT16_MAX;
-    g_adc = ulp_adc_reading;
-
-    return g_adc;
+    
+    return g_temp;
 }
 
 esp_err_t app_sensor_init()
@@ -104,4 +116,19 @@ esp_err_t app_sensor_init()
 void app_driver_init()
 {
     app_sensor_init();
+}
+
+//converts c string to string, required parameter for JSON 
+void floatArrayToString(uint32_t arr[], int size, char *str) {
+    int i;
+    strcat(str, "[");
+    for (i = 0; i < size; i++) {
+        char temp[100]; //MAX size set to 100
+        snprintf(temp, sizeof(temp), "%"PRIu32"", arr[i]); // Format float to string with 2 decimal places
+        strcat(str, temp);
+        if (i < size - 1) {
+            strcat(str, ", ");
+        }
+    }
+    strcat(str, "]");
 }
